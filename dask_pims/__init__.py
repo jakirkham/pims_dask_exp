a4f578cb65307bbbe1a0be464d8e574d60fec865
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-04-12 13:56:34 -0400

    Add a simple `imread` function
    Use PIMS to read in image files and makes them available as a Dask
    array. Does per frame chunking of the image files.

---
+++ dask_pims/__init__.py
@@ -0,0 +1,26 @@
+import dask
+import dask.array
+import dask.delayed
+import numpy
+import pims
+
+
+def imread(fn):
+    with pims.open(fn) as imgs:
+        shape = (len(imgs),) + imgs.frame_shape
+        dtype = numpy.dtype(imgs.pixel_type)
+
+    def _read_frame(fn, i):
+        with pims.open(fn) as imgs:
+            return imgs[i]
+
+    a = []
+    for i in range(shape[0]):
+        a.append(dask.array.from_delayed(
+            dask.delayed(_read_frame)(fn, i),
+            shape[1:],
+            dtype
+        ))
+    a = dask.array.stack(a)
+
+    return a
